5353d4bbc06a2296a2bae5bcfad01f8686bfd168
Artur de Sousa <artursv2001@gmail.com>
2023-02-01 19:50:22 -0300

    modificado base do projeto front

--- PokeTeam/poketeam_frontend/src/app/app.component.spec.ts
+++ PokeTeam/poketeam_frontend/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pokecartesian'`, () => {
+  it(`should have as title 'poketeam'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pokecartesian');
+    expect(app.title).toEqual('poketeam');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('pokecartesian app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('poketeam app is running!');
   });
 });
